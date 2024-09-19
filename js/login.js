30f4a3799ff0c1ba1e19834f145b0cd5ea6c1084
Munia123333t <muniaakhter566@gmail.com>
2024-09-19 10:55:21 +0600

    complete form section

--- js/login.js
+++ js/login.js
@@ -1,1 +1,17 @@
-console.log('my name')
+// console.log('my name')
+document.getElementById('btn-login')
+.addEventListener('click',function(event){
+    event.preventDefault()
+    console.log('login button click')
+    // get phone and pin number
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber , pinNumber);  
+
+    if(phoneNumber === '01633'&& pinNumber === '1234'){
+        console.log('you are login');
+    }
+    else{
+        alert('wrong phone number or pin');
+    }
+})
